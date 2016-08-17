1889a226c0e217aa0a45894428f8818e4db69e6c
john-atkinson-oci <john.atkinson@owlcomputing.com>
2016-08-17 13:17:22 -0400

    Updated to include changed statuses and to use a different default status icon so as to avoid giving false positive data

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 // Note: when running in Voyager, this static file is partially overwritten with voyager config settings
 /* jshint ignore:start */
 var config = {
-    //'root': 'http://voyagerdemo.com/daily/',
-    'root': 'http://voyagerdemo.com/198/',
+    'root': 'http://voyagerdemo.com/daily/',
+    //'root': 'http://voyagerdemo.com/198/',
     //'root': 'http://localhost:8888/',
     'explorePath': 'voyager',
     'enableEmail': true,
@@ -81,6 +81,6 @@ var config = {
     excludeDetails : ['_', 'allow_', 'deny_', 'http_header', 'tree', 'links', 'geo', 'path_to_', 'debug_properties', 'linkcount_', 'hasThumb', 'hasPreview', 'hasMetadata', 'md5', 'worker', 'extractor', 'hasLayerFile', 'id', 'extent', 'root', 'indexing_warning'],
     chartData: 'http://voyagerdemo.com/sql/solr/v0/select?q=*:*&fl=name,meta_Field_Name,meta_Operator,meta_Producing,fl_Well_Depth,meta_Well_Statu,%20meta_Well_Type&wt=json&rows=1000&json.wrf=JSON_CALLBACK',
     rememberMe: true,
-    editAll: true
+    editAll: false
 };
 /* jshint ignore:end */
